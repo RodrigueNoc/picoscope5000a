329a4a11b94bc67d6cfedd1f5dc640765f080fca
BuildTools <unconfigured@null.spigotmc.org>
2024-12-08 19:59:28 +0100

    First version of app

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ import picoscope as ps
 Pico = ps.Picoscope()
 
 # Initialisation
+# Start Pico
 Pico.StartPico()
+# Start Streaming
 Pico.SetChannel('A',1,"DC","2V",0.)
 Pico.SetTrigger(1,-100,"FALLING",0,0)
 Pico.StartStreaming(10000,"NONE",1,0,50,'US')
 
-# 
+# Get values
 RUN = True
 while RUN:
     Pico.GetStreamingLatestValues()
@@ -21,6 +23,7 @@ while RUN:
         Y = Pico.y_mV
         X = np.linspace(0, Pico.buffer_size*Pico.sampleInterval.value, Pico.buffer_size)
 
-# Stop
+# Stop Streaming
 Pico.StopStreaming()
+# Stop Pico
 Pico.StopPico()

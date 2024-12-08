dedba41c0ee23fc1197c052fff18931c7b341273
BuildTools <unconfigured@null.spigotmc.org>
2024-12-08 16:29:33 +0100

    picoscope.py update pep8

--- picoscope.py
+++ picoscope.py
@@ -11,9 +11,12 @@ class Picoscope():
         Initialise toutes les constantes pour le picoscope
         """
         self.status = {}
-        self.ps_voie = {}
 
-    def StartPico(self, serial=None, resolution=1):
+    def StartPico(
+            self,
+            serial=None,
+            resolution=1
+    ):
         """
         Fonction qui appel OpenUnit p75, 4.37
 
@@ -29,11 +32,20 @@ class Picoscope():
         self.resolution = resolution
 
         self.status['OpenUnit'] = ps.ps5000aOpenUnit(
-            ctypes.byref(self.handle), self.serial, self.resolution)
+            ctypes.byref(self.handle),
+            self.serial,
+            self.resolution
+        )
         assert_pico_ok(self.status['OpenUnit'])
 
-    def Channel(self, Cha="A", channel_enabled=1, channel_type='DC',
-                channel_range="2V", analogueOffset=0.):
+    def Channel(
+            self,
+            Cha="A",
+            channel_enabled=1,
+            channel_type='DC',
+            channel_range="2V",
+            analogueOffset=0.
+    ):
         """
         Fonction qui paramètre les voies du picoscope p84, 4.46
 
@@ -47,14 +59,20 @@ class Picoscope():
             "200MV", "500MV", "1V", "2V", "5V", "10V", "20V", "50V"
 
         """
-        ps_channel = ps.PS5000A_CHANNEL[f'PS5000A_CHANNEL_{Cha}']
-        ps_channel_type = ps.PS5000A_COUPLING[f'PS5000A_{channel_type}']
-        channel_enabled = channel_enabled
-        ps_channel_range = ps.PS5000A_RANGE[f'PS5000A_{channel_range}']
-        analogueOffset = analogueOffset
-        self.ps_voie[Cha] = [ps_channel, ps_channel_type, channel_enabled, ps_channel_range, analogueOffset]
+        self.ps_channel = ps.PS5000A_CHANNEL[f'PS5000A_CHANNEL_{Cha}']
+        self.ps_channel_type = ps.PS5000A_COUPLING[f'PS5000A_{channel_type}']
+        self.channel_enabled = channel_enabled
+        self.ps_channel_range = ps.PS5000A_RANGE[f'PS5000A_{channel_range}']
+        self.analogueOffset = analogueOffset
 
-        self.status['SetChannel'] = ps.ps5000aSetChannel(self.handle, ps_channel, channel_enabled, ps_channel_type, ps_channel_range, analogueOffset)
+        self.status['SetChannel'] = ps.ps5000aSetChannel(
+            self.handle,
+            self.ps_channel,
+            self.channel_enabled,
+            self.ps_channel_type,
+            self.ps_channel_range,
+            analogueOffset
+        )
         assert_pico_ok(self.status['SetChannel'])
 
     def MAX_ADC(self):
@@ -62,11 +80,21 @@ class Picoscope():
         Appel la fonction ps5000aMaximumValue p70, 4.32
         """
         self.maxADC = ctypes.c_int16()
-        
-        self.status["maximumValue"] = ps.ps5000aMaximumValue(self.handle, ctypes.byref(self.maxADC))
+
+        self.status["maximumValue"] = ps.ps5000aMaximumValue(
+            self.handle,
+            ctypes.byref(self.maxADC)
+        )
         assert_pico_ok(self.status["maximumValue"])
 
-    def SetTrigger(self, trigger_enabled = 1, Cha = "A", seuil = 1000, direction = "FALLING", trigger_delay = 0, trigger_autoTrigger_ms = 0):
+    def SetTrigger(
+            self,
+            trigger_enabled=1,
+            seuil=1000,
+            direction="FALLING",
+            trigger_delay=0,
+            trigger_autoTrigger_ms=0
+    ):
         """
         Fonction qui initialise un trigger p115, 4.66
 
@@ -79,13 +107,33 @@ class Picoscope():
         trigger_autoTrigger_ms : durée d'attente d'un trigger; 0 = infini
         """
         self.MAX_ADC()
-        trigger_threshold = mV2adc(seuil, self.ps_voie[Cha][3], self.maxADC)
+        trigger_threshold = mV2adc(
+            seuil,
+            self.ps_channel_range,
+            self.maxADC
+        )
         trigger_direction = ps.PS5000A_THRESHOLD_DIRECTION[f'PS5000A_{direction}']
-        
-        self.status['SetSimpleTrigger'] = ps.ps5000aSetSimpleTrigger(self.handle, trigger_enabled, self.ps_voie[Cha][0], trigger_threshold, trigger_direction, trigger_delay, trigger_autoTrigger_ms)
+
+        self.status['SetSimpleTrigger'] = ps.ps5000aSetSimpleTrigger(
+            self.handle,
+            trigger_enabled,
+            self.ps_channel,
+            trigger_threshold,
+            trigger_direction,
+            trigger_delay,
+            trigger_autoTrigger_ms
+        )
         assert_pico_ok(self.status['SetSimpleTrigger'])
 
-    def StartStreaming(self, Cha = "A", buffer_size = 1000, mode = "NONE", Ratio = 1, autoStop = 0, intervale = 1, unite = 'NS'):
+    def StartStreaming(
+            self,
+            buffer_size=1000,
+            mode="NONE",
+            Ratio=1,
+            autoStop=0,
+            intervale=1,
+            unite='NS'
+    ):
         """
         Fonction qui commence l'acquisition de données
 
@@ -96,17 +144,27 @@ class Picoscope():
             "DECIMATE", "AVERAGE"
         Ratio : valeur de n dans les regroupement du mode (cf p59)
         autoStop : indique si le Stream doit s'arrêter
-        intervale : donne le temps de l'intervale de mesure (unitée préciser après)
+        intervale : donne le temps de l'intervale de mesure
         unite : p54, 4.18.3; unitée d'un intervale de mesure;
             possible values : "FS", "PS", "NS", "US", "MS", "S"
         """
         # Initialisation du stockage
         self.buffer_size = buffer_size
-        self.buffer_data = np.zeros(shape=buffer_size, dtype=np.int16)
+        self.buffer_data = np.zeros(
+            shape=buffer_size,
+            dtype=np.int16
+        )
         self.mode = ps.PS5000A_RATIO_MODE[f'PS5000A_RATIO_MODE_{mode}']
         downSampleRatio = Ratio
 
-        self.status['SetDataBuffer'] = ps.ps5000aSetDataBuffer(self.handle, self.ps_voie[Cha][0], self.buffer_data.ctypes.data_as(ctypes.POINTER(ctypes.c_int16)), self.buffer_size, 0, self.mode)
+        self.status['SetDataBuffer'] = ps.ps5000aSetDataBuffer(
+            self.handle,
+            self.ps_channel,
+            self.buffer_data.ctypes.data_as(ctypes.POINTER(ctypes.c_int16)),
+            self.buffer_size,
+            0,  # segmentIndex
+            self.mode
+        )
         assert_pico_ok(self.status['SetDataBuffer'])
 
         # Début du streaming
@@ -116,7 +174,17 @@ class Picoscope():
         self.sampleInterval = ctypes.c_int32(intervale)
         self.sampleIntervalTimeUnits = ps.PS5000A_TIME_UNITS[f'PS5000A_{unite}']
 
-        self.status['StartStreaming'] = ps.ps5000aRunStreaming(self.handle, ctypes.byref(self.sampleInterval), self.sampleIntervalTimeUnits, maxPreTriggerSamples, maxPostTriggerSamples, self.autoStop, downSampleRatio, self.mode, self.buffer_size)
+        self.status['StartStreaming'] = ps.ps5000aRunStreaming(
+            self.handle,
+            ctypes.byref(self.sampleInterval),
+            self.sampleIntervalTimeUnits,
+            maxPreTriggerSamples,
+            maxPostTriggerSamples,
+            self.autoStop,
+            downSampleRatio,
+            self.mode,
+            self.buffer_size
+        )
         assert_pico_ok(self.status['StartStreaming'])
 
     def StopStreaming(self):
